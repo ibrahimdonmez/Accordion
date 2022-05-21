e6bac409ca42155831a0e91d585bd3f4f81b1063
ibrahimdonmez <empayfi@gmail.com>
2022-05-21 16:46:52 +0300

    Init

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'Çerez kullanımına izin vermem gerekiyor mu ?',
+    info:
+      'Hayır.',
+  },
+  {
+    id: 2,
+    title: 'Şifremi nasıl değiştiririm ?',
+    info:
+      'Şifremi unuttum butonuna basarak şifrenizi sıfırlayabilirsiniz.',
+  },
+  {
+    id: 3,
+    title: 'BankID Nedir ?',
+    info:
+      'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+  },
+  {
+    id: 4,
+    title: 'Kimin doğum gününü girebilirim ?',
+    info:
+      'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+  },
+  {
+    id: 5,
+    title: 'Şifremi sıfırladığım zaman ne zaman değiştirebilirim ?',
+    info:
+      'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+  },
+]
+export default questions
